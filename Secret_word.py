5ebe1e4423ca776bccf0689a112234044d05f3d6
Vinicius De Carvalho <74793351+carvalhov@users.noreply.github.com>
2021-09-26 02:31:08 -0300

    Update Secret_word.py

--- Secret_word.py
+++ Secret_word.py
@@ -1,29 +1,32 @@
 print("Bem vindo ao jogo, PALAVRA SECRETA!\n")
 
-dicas = { # criação de uma 
+dicas = { # criação de um dictionarie de strings para strings
+    #key -> correspondente
     "1" : "Pode ser um detetive", #Perry o ornintorrinco haha 
     "2" : "É um animal",
     "3" : "É um mamifero"
 }
-
-secret_word = "ornitorrinco"
+secret_word = "ornitorrinco" 
 palpite = ""
-cont_palpites = 0
-limite_palpites = 5
-end_game = False
+cont_palpites = 0 #conta os palpites utilizados (int)
+limite_palpites = 5 #estabelece um limite para os palpites (int)
+end_game = False 
+#bool para estabelecer o fim do jogo, naturalmente, antes do começo, o fim de jogo é falso 
 
 while secret_word != palpite and not(end_game):
+#a repetição ocorre, enquanto o palpite não bate com a palavra secreta e, enquanto end_game = false
     if limite_palpites > cont_palpites:
         palpite = input("Digite o seu palpite: ")
+#Caso digite os valores key, chama os valores correspondentes no dicionário
         if palpite == "1":
             print(dicas.get("1"))
         elif palpite == "2":
             print(dicas.get("2"))
         elif palpite == "3":
             print(dicas.get("3"))
-        cont_palpites += 1
-        print("Palpites restantes: " + str(limite_palpites - cont_palpites))
-    else:
+        cont_palpites += 1 #conta os palpites gastos
+        print("Palpites restantes: " + str(limite_palpites - cont_palpites)) #Mostra os palpites restantes
+    else: #Quando limite_palpites deixa de ser maior que cont_palpites, end_game = True e o while se encerra 
         end_game = True
     
 if end_game:
